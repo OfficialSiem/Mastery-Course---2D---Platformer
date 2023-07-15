e08fab0c6c8872a682d1a4c7452910604cc8d9d7
Siem <siemgdeveloper@gmail.com>
2023-07-15 09:44:05 -0400

    Added Speed to SawBlade
    SawBlade travels at a constant rate between two positions
    SawBlade now a prefab

--- Assets/_Scripts/SawBlade.cs
+++ Assets/_Scripts/SawBlade.cs
@@ -10,7 +10,9 @@ public class SawBlade : MonoBehaviour
     private Transform end;
     [SerializeField]
     private Transform sawBladeSprite;
-    
+    [SerializeField]
+    private float speed = 1;
+
     private float positionPercent;
     private int direction = 1;
 
@@ -18,7 +20,17 @@ public class SawBlade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        positionPercent += Time.deltaTime * direction;
+        //The distance between start and end will help us determine...
+        float distance = Vector3.Distance(start.position, end.position);
+
+        // At what constant rate the saw must travel..
+        float speedForDistance = speed/distance;
+
+        // knowing the rate will help calculate how far along the saw is in its journey
+        positionPercent += Time.deltaTime * direction * speedForDistance;
+
+        // We lerp the saw's position accounting for the overall percentage of its total trek
+        // (0% or 0.0, still at start, 100% or 1.0, finally reached the end) 
 
         //Lerping, or 'linear interpolation' is a formula to find a point between two numbers given
         // a percentage value. - Lerping in Unity - "You HAVE to know this!!" - Tarodev - Sep 9, 2021 (https://www.youtube.com/watch?v=JS7cNHivmHw&t)
@@ -29,9 +41,10 @@ public class SawBlade : MonoBehaviour
             direction = -1;
         }
         else if (positionPercent <= 0 && direction == -1){
+            //Once we hit the end, turn around and start moving backwards lol
             direction = 1;
         }
 
-        Debug.Log(positionPercent);
+
     }
 }
